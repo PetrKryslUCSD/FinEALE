847e859e173be5a1f0820054dbe214cebbb9371f
Petr <pkrysl@ucsd.edu>
2016-01-24 16:25:29 -0800

    Much faster implementation of the connection matrix

--- fineale/classes/femm/@femm_base/connection_matrix.m
+++ fineale/classes/femm/@femm_base/connection_matrix.m
@@ -12,14 +12,19 @@ function S = connection_matrix (self, assembler, u)
 % Return a matrix representing the connections between the finite element nodes
 % expressed by the  finite elements.
     
-% Retrieve data for efficiency
-    tconns =transpose(self.fes.conn);% connectivity, Transposed for efficiency
-    % Prepare assembler
-    Kedim =self.fes.nfens; Ke = ones(Kedim,Kedim);
-    start_assembly(assembler, Kedim, Kedim, size(tconns,2), u.nfens, u.nfens);
-    % Now loop over all fes in the block
-    for i=1:size(tconns,2)
-        assemble_symmetric(assembler, Ke, tconns(:,i));
-    end
-    S = make_matrix (assembler);
+% % Retrieve data for efficiency
+%     tconns =transpose(self.fes.conn);% connectivity, Transposed for efficiency
+%     % Prepare assembler
+%     Kedim =self.fes.nfens; Ke = ones(Kedim,Kedim);
+%     start_assembly(assembler, Kedim, Kedim, size(tconns,2), u.nfens, u.nfens);
+%     % Now loop over all fes in the block
+%     for i=1:size(tconns,2)
+%         assemble_symmetric(assembler, Ke, tconns(:,i));
+%     end
+%     S = make_matrix (assembler);
+    
+        B = repmat(self.fes.conn,1,1,size(self.fes.conn,2));
+        rb = permute(B,[2,3,1]);
+        cb = permute(B,[3,2,1]);
+        S = sparse(rb(:), cb(:), ones(size(self.fes.conn,2)*size(self.fes.conn,2)*size(self.fes.conn,1),1), u.nfens, u.nfens);
 end
