090c528f8afb597910a49c8d70faaa731640223a
Petr <pkrysl@ucsd.edu>
2016-04-01 10:27:59 -0700

    Update of the runners and documentation

--- fineale/util/runners/Contents.m
+++ fineale/util/runners/Contents.m
@@ -1,6 +1,7 @@
 % RUNNERS
 %
 % Files
+%   abaqus_job       - Run the Abaqus executable.   
 %   paraview         - Run the Paraview paraview executable.
 %   pvpython         - Run the Paraview pvpython executable.
 %   run_all_examples - Run all in examples in the current directory and its subdirectories.
--- fineale/util/runners/abaqus_job.m
+++ fineale/util/runners/abaqus_job.m
@@ -3,10 +3,12 @@ function Status=abaqus_job(inpfile)
     %
     % function Status=abaqus_job(inpfile)
     %
-    %
+    % The Abaqus executable is proprietary to SIMULIA. If you have access
+    % to it, it is possible to run it from within FinEALE solution files.
+    % One may have to input the complete path to the executable. See below.
     
     if ispc 
-        executable = 'abaqus';
+        executable = 'C:\SIMULIA\Abaqus\6.14-2SE\code\bin\abq6142se.exe';
     elseif isunix
         executable = 'abaqus';
     else
