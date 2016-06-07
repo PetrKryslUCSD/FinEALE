9b3d23edebc92db7ac23c72ff2f30a73655c12d5
Petr <pkrysl@ucsd.edu>
2016-06-06 19:33:03 -0700

    The log() function blows up  for some inputs: try and catch

--- fineale/util/graphics_utilities/set_decades_on_axis.m
+++ fineale/util/graphics_utilities/set_decades_on_axis.m
@@ -6,14 +6,18 @@ end
 if (strcmp(which,'all' ))||(strcmp(which,'x' ))
     xlim=get(ax,'xlim');
     xlim=10.^[floor(log10(xlim(1))),ceil(log10(xlim(2)))];
-    set(ax,'xlim',xlim);
-    set(ax,'xtick',10.^[floor(log10(xlim(1))):ceil(log10(xlim(2)))]);
+    try
+        set(ax,'xlim',xlim);
+        set(ax,'xtick',10.^[floor(log10(xlim(1))):ceil(log10(xlim(2)))]);
+    catch,end
 end
 
 if (strcmp(which,'all' ))||(strcmp(which,'y' ))
     ylim=get(ax,'ylim');
     ylim=10.^[floor(log10(ylim(1))),ceil(log10(ylim(2)))];
-    set(ax,'ylim',ylim);
-    set(ax,'ytick',10.^[floor(log10(ylim(1))):ceil(log10(ylim(2)))]);
+    try
+        set(ax,'ylim',ylim);
+        set(ax,'ytick',10.^[floor(log10(ylim(1))):ceil(log10(ylim(2)))]);
+    catch,end
 end
 
