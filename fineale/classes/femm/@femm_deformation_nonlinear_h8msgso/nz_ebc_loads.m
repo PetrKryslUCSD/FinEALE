7ba97cc5b7e35200f4f4b4f8a2b8db31d24bde14
Petr <pkrysl@ucsd.edu>
2016-06-12 15:50:04 -0700

    Bug fixes

--- fineale/classes/femm/@femm_deformation_nonlinear_h8msgso/nz_ebc_loads.m
+++ fineale/classes/femm/@femm_deformation_nonlinear_h8msgso/nz_ebc_loads.m
@@ -1,20 +1,30 @@
-function F = nz_ebc_loads(self, assembler, geom, u1)
+function F = nz_ebc_loads(self, assembler, geom, un1, un, dt)
 % Compute load vector for nonzero essential boundary conditions.
 %
-% function F = nz_ebc_loads(self, assembler, geom, u)
+% function F = nz_ebc_loads(self, assembler, geom, un1, un, dt)
 %
-%
-% Return the assembled system vector F.
-%    Arguments
-%     assembler =  descendent of sysvec_assembler
+% Return the load vector matrix corresponding to prescribed displacements.
+%     assembler = descendent of the sysvec_assembler class
 %     geom=geometry field
-%     u=displacement field
+%     un1      - displacement field at the end of time step t_n+1
+%     un       - displacement field at the end of time step t_n
+%     dt       - time step from  t_n to t_n+1; needed only by some
+%                materials
 %
 
-if isempty(self.phis)%  Do we need to calculate the form factors?
-    self=self.update(geom, u1, u1);
+if (~exist('dt','var'))
+    % If the time step is needed, this will cause trouble.  Which indicates
+    % it should have been supplied. 
+    dt=[];
 end
-
+if (~exist('un','var'))
+    % When this method gets called in linear problems, no previous
+    % deformation is supplied: make one up.
+    un=0*un1;
+end
+if isempty(self.phis)%  Do we need to calculate the form factors?
+    error('Need the stabilization parameters');
+end
 % Integration rule
 [npts, Ns, gradNparams, w] = integration_data (self);;
 gradN =cell(8,1); Jac=cell(8,1);
@@ -30,24 +40,26 @@ end
 conns = self.fes.conn; % connectivity
 labels = self.fes.label; % connectivity
 Xs = geom.values; % reference coordinates
-% Us = u.values; % displacement in step n
-U1s = u1.values; % displacement in step n+1
-context.F = [];
+Uns = un.values; % displacement in step n
+Un1s = un1.values; % displacement in step n+1
 context.stiff_type='Eulerian';
+context.dt=dt;% time step
 % Prepare assembler
-Kedim =u1.dim*self.fes.nfens;
-start_assembly(assembler, u1.nfreedofs);
+Kedim =un1.dim*self.fes.nfens;
+start_assembly(assembler, un1.nfreedofs);
 for i=1:size(conns,1)
     conn =conns(i,:); % connectivity
-    pu = reshape(u1, gather_fixed_values(u1, conn));
+    pu = reshape(un1, gather_fixed_values(un1, conn));
     if norm (pu) ~= 0
         X=Xs(conn,:);
-        U1=U1s(conn,:);
-        x1 = X + U1; % current coordinates
-        dofnums =reshape(u1,gather_dofnums(u1,conn));
+        Un=Uns(conn,:);
+        Un1=Un1s(conn,:);
+        xn = X + Un; % previous known coordinates
+        xn1 = X + Un1; % current coordinates
+        dofnums =reshape(un1,gather_dofnums(un1,conn));
         Ke =zeros(Kedim);;
         % First we calculate  the mean basis function gradient matrix and the volume of the element
-        gradN_mean =zeros(self.fes.nfens,u1.dim); V=0;
+        gradN_mean =zeros(self.fes.nfens,un1.dim); V=0;
         for j=1:npts % loop over all quadrature points
             J = X' * gradNparams{j};% Jacobian matrix wrt reference coordinates
             gradN{j} = gradNparams{j}/J;% derivatives wrt global reference coor
@@ -64,10 +76,12 @@ for i=1:size(conns,1)
             else,                    Rm =Rmh(c,[],[]);                end
         end
         % Now we calculate the mean deformation gradient
-        F1bar =x1' * gradN_mean;
-        Bbar = self.hBlmat(self,[],gradN_mean/F1bar,[],[]);% strain-displacement d/dX
+        Fnbar =xn' * gradN_mean;
+        Fn1bar =xn1' * gradN_mean;
+        Bbar = self.hBlmat(self,[],gradN_mean/Fn1bar,[],[]);% strain-displacement d/dX
+        context.Fn=Rm'*Fnbar*Rm;%  Deformation gradient in material coordinates
+        context.Fn1=Rm'*Fn1bar*Rm;%  Deformation gradient in material coordinates
         context.ms=self.matstates{i};
-        context.F=Rm'*F1bar*Rm;%  Deformation gradient in material coordinates
         D = tangent_moduli (self.material, context);
         D = rotate_stiffness(self.material,D,Rm');
         Ke = (Bbar'*(D*(V))*Bbar);
@@ -76,9 +90,9 @@ for i=1:size(conns,1)
         Dstab = rotate_stiffness(self.stabilization_material,Dstab,Rm');
         Ke = Ke - (Bbar'*(Dstab*(self.phis(i)*V))*Bbar);
         for j=1:npts       % Loop over all integration points
-            F1 = x1'*gradN{j};% Current deformation gradient
-            B = self.hBlmat(self,[],gradN{j}/F1,[],[]);% strain-displacement
-            context.F=Rm'*F1*Rm;
+            Fn1 = xn1'*gradN{j};% Current deformation gradient
+            B = self.hBlmat(self,[],gradN{j}/Fn1,[],[]);% strain-displacement
+            context.Fn1=Rm'*Fn1*Rm;
             Dstab = tangent_moduli (self.stabilization_material, context);
             Dstab = rotate_stiffness(self.stabilization_material,Dstab,Rm');
             Ke = Ke + (B'*(Dstab*(self.phis(i)*Jac{j}*w(j)))*B);
