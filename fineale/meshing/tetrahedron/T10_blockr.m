4541b1acfe6f2108d30ca3fd0a3a3bfe5ad11a50
PetrKryslUCSD <pkrysl@ucsd.edu>
2016-07-04 13:38:38 +0200

    Documentation update

--- fineale/meshing/tetrahedron/T10_blockr.m
+++ fineale/meshing/tetrahedron/T10_blockr.m
@@ -1,11 +1,12 @@
 function [fens,fes] = T10_blockr(Length,Width,Height,nL,nW,nH,hT4_block)
-% Tetrahedral (T10) mesh of a rectangular block.
+% Tetrahedral (T10) mesh of a rectangular block using the function handle.
 %
 % function [fens,fes] = T10_blockr(Length,Width,Height,nL,nW,nH)
 %
 % Range =<0,Length> x <0,Width> x <0,Height>
 % Divided into elements: nL, nW, nH in the first, second, and
 % third direction (x,y,z).
+% hT4_block=handle to a T4 generation function
 %
 % See also: T10_blocka, T10_blockb, T10_blockca, T10_blockcb
 
